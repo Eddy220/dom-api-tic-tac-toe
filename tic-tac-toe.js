a7d7e62c2766476b61d0d0f69e10272a2945a13b
Drew Long <drewlong314@gmail.com>
2021-03-04 16:59:43 -0600

    added images

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,20 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  let squares = document.querySelectorAll(".square");
-  squares.forEach(squareEle => {
-    squareEle.addEventListener("click", (event) => {
-      let xImage = document.createElement("img")
-      xImage.src = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg"
-      console.log(squareEle);
-    })
+  let currentPlayerSymbol = 'x';
+  let squareValues = ['', '', '', '', '', '', '', '', '']
+  let xImage = document.createElement('img')
+  xImage.setAttribute('src', "player-x.svg")
+  let oImage = document.createElement('img')
+  oImage.setAttribute('src', 'player-o.svg')
+
+
+  let board = document.getElementById('tic-tac-toe-board')
+  board.addEventListener('click', event => {
+    console.log(event.target)
+    let clicked = event.target.id
+    if (clicked.includes('square-')) {
+
+      // xImage.src = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg."
+      event.target.appendChild(xImage)
+    }
   })
 });
