5e3410b4f9c85ad92439b50f1faf5be1684e5e32
Drew Long <drewlong314@gmail.com>
2021-03-04 17:59:51 -0600

    kinda works

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -9,7 +9,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   let board = document.getElementById('tic-tac-toe-board')
   board.addEventListener('click', event => {
-    console.log(event.target)
     let clicked = event.target.id
     if (clicked.includes('square-')) {
       if (currentPlayerSymbol === 'x') {
@@ -17,14 +16,33 @@ document.addEventListener("DOMContentLoaded", (event) => {
         xImage.setAttribute('src', "player-x.svg")
         event.target.appendChild(xImage);
         currentPlayerSymbol = 'o'
-        console.log(currentPlayerSymbol)
+        checkGameStatus()
       } else if (currentPlayerSymbol === 'o') {
           let oImage = document.createElement('img')
           oImage.setAttribute('src', 'player-o.svg')
           event.target.appendChild(oImage);
           currentPlayerSymbol = 'x'
-          console.log(currentPlayerSymbol);
+          checkGameStatus()
       }
+
     }
   })
+let gameResult1 = document.getElementById('game-status')
+
+  const checkGameStatus = () => {
+    const square0 = document.getElementById('square-0')
+    const square1 = document.getElementById('square-1')
+    const square2 = document.getElementById('square-2')
+    const square3 = document.getElementById('square-3')
+    const square4 = document.getElementById('square-4')
+    const square5 = document.getElementById('square-5')
+    const square6 = document.getElementById('square-6')
+    const square7 = document.getElementById('square-7')
+    const square8 = document.getElementById('square-8')
+
+    console.log(square0.firstElementChild)
+    if (square0.firstElementChild === square1.firstElementChild && square0.firstElementChild === square2.firstElementChild) {
+      gameResult1.innerText = 'Winner: X'
+    }
+  }
 });
